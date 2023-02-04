d900751ce9bd15314b3d77b069c1adb9306dede5
Ilya Makarov <ilyam2003@mail.ru>
2023-02-04 20:35:18 +0500

    Buttons Remuved

--- create.py
+++ create.py
@@ -1,14 +1,14 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-kb_main_menu = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+# kb_main_menu = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
 
-btn_settings = KeyboardButton('/settings')
-btn_help = KeyboardButton('/help')
-btn_rules = KeyboardButton('/rules')
+# btn_settings = KeyboardButton('/settings')
+# btn_help = KeyboardButton('/help')
+# btn_rules = KeyboardButton('/rules')
 
-kb_main_menu.add(btn_settings,btn_help)
-kb_main_menu.add(btn_rules)
+# kb_main_menu.add(btn_settings,btn_help)
+# kb_main_menu.add(btn_rules)
 
 
 key_file = open('key','r')
